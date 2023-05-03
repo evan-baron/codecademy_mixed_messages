847ad8f14443ac787f314217ad6d764c6661dc84
evan baron <egbaron@gmail.com>
2023-05-03 11:45:01 -0600

    added js script script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // script for button
+let curQuoteId = '';
+
+function generateQuote() {
+    let randomId = (Math.floor(Math.random() * 10) + 1);
+    console.log(randomId);
+    
+    document.getElementById('quote-container').style.display = 'block';
+
+    // hide currently showing quote
+    if (curQuoteId != '') {
+        // there is a prior quote showing
+        document.getElementById(curQuoteId).style.display = 'none';
+    }
+        // showing the new quote
+    document.getElementById(randomId).style.display = 'block';
+    curQuoteId = randomId;
+}
+
